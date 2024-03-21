63fedd850732545545a79d088dd6a2537ef66d47
shakyapeiris <shakya.peiris@octopusbi.com>
2024-03-21 13:00:50 +0530

    Implement linked list

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,49 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
+pub mod linked_list;
 
 #[cfg(test)]
 mod tests {
+    use self::linked_list::Node;
+
     use super::*;
 
     #[test]
     fn it_works() {
-        let result = add(2, 2);
+        let result = 2 + 2;
         assert_eq!(result, 4);
     }
+
+    fn get_new_value<T>(head: &Node<T>) -> &Option<T>{
+        match &head.next {
+            Some(node) => get_new_value(&node),
+            None => &head.value
+        }
+    }
+
+    #[test]
+    fn create_a_new_node() {
+        let new_node = linked_list::Node::new(Some(10));
+
+        assert_eq!(new_node.next, None);
+        assert_eq!(new_node.value, Some(10));
+    }
+
+    #[test] 
+    fn inserts_a_new_node(){
+        let mut new_node = linked_list::Node::new(Some(10));
+        new_node.insert(Some(100));
+
+        let newly_inserted_value = get_new_value(&new_node);
+        assert_eq!(*newly_inserted_value, Some(100))
+    }
+
+    #[test]
+    fn reverse_a_list() {
+        let mut new_node = linked_list::Node::new(Some(10));
+        new_node.insert(Some(100));
+        new_node.insert(Some(200));
+
+        let new_head = new_node.reverse(None);
+        assert_eq!(new_head.value, Some(200))
+    }
+    
 }
---
+++ src/linked_list.rs
@@ -0,0 +1,51 @@
+#[derive(PartialEq, Debug, Clone)]
+pub struct Node<T> {
+    pub next: Option<Box<Node<T>>>,
+    pub value: Option<T>
+}
+
+impl<T> Node<T> {
+    pub fn new(value: Option<T>) -> Node<T> {
+        Node {
+            next: None,
+            value
+        }
+    }
+
+    pub fn insert(&mut self, value: Option<T>){
+        match &mut self.next {
+            Some(node) => {
+                node.insert(value)
+            },
+            None => {
+                let new_node = Node {
+                    next: None,
+                    value
+                };
+                self.next = Some(Box::new(new_node))
+            }
+        }
+    }
+
+    pub fn reverse(self, prev: Option<Box<Node<T>>>) -> Node<T>{
+        let next = self.next;
+        // if next none, then it's the tail so return the new head
+        // else previous will be the current one, current will be the new one
+        match next {
+            Some(node) => {
+                let new_node = Node {
+                    next: prev,
+                    value: self.value
+                };
+                node.reverse(Some(Box::new(new_node)))
+            },
+            None => {
+                Node {
+                    next: prev,
+                    value: self.value
+                }
+            }
+        }
+
+    }
+}
